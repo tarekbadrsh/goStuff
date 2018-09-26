6becf9702cf19da579a91f6514a21f7e10e5ba69
tarekbadr <tarekbadrshalaan@gmail.com>
2018-09-26 15:06:47 +0200

    add base64 safe url

--- numbercompression/numbercompression.go
+++ numbercompression/numbercompression.go
@@ -2,7 +2,10 @@ package numbercompression
 
 //Ref https://stackoverflow.com/questions/5901153/compress-large-integers-into-smallest-possible-string
 
-var defaultEncodeDic = map[int64]string{
+//base64::urlsafe()
+//Base64 := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_"
+
+var defaultEncodeDic = map[int]string{
 	0:  "A",
 	1:  "B",
 	2:  "C",
@@ -11,49 +14,65 @@ var defaultEncodeDic = map[int64]string{
 	5:  "F",
 	6:  "G",
 	7:  "H",
-	8:  "J",
-	9:  "K",
-	10: "M",
-	11: "N",
-	12: "P",
-	13: "Q",
-	14: "R",
-	15: "S",
-	16: "T",
-	17: "U",
-	18: "V",
-	19: "W",
-	20: "X",
-	21: "Y",
-	22: "Z",
-	23: "a",
-	24: "b",
-	25: "c",
-	26: "d",
-	27: "e",
-	28: "f",
-	29: "g",
-	30: "h",
-	31: "j",
-	32: "k",
-	33: "m",
-	34: "n",
-	35: "p",
-	36: "q",
-	37: "r",
-	38: "s",
-	39: "t",
-	40: "u",
-	41: "v",
-	42: "x",
-	43: "y",
-	44: "z",
-	45: "2",
-	46: "3",
-	47: "4",
+	8:  "I",
+	9:  "J",
+	10: "K",
+	11: "L",
+	12: "M",
+	13: "N",
+	14: "O",
+	15: "P",
+	16: "Q",
+	17: "R",
+	18: "S",
+	19: "T",
+	20: "U",
+	21: "V",
+	22: "W",
+	23: "X",
+	24: "Y",
+	25: "Z",
+	26: "a",
+	27: "b",
+	28: "c",
+	29: "d",
+	30: "e",
+	31: "f",
+	32: "g",
+	33: "h",
+	34: "i",
+	35: "j",
+	36: "k",
+	37: "l",
+	38: "m",
+	39: "n",
+	40: "o",
+	41: "p",
+	42: "q",
+	43: "r",
+	44: "s",
+	45: "t",
+	46: "u",
+	47: "v",
+	48: "w",
+	49: "x",
+	50: "y",
+	51: "z",
+	52: "0",
+	53: "1",
+	54: "2",
+	55: "3",
+	56: "4",
+	57: "5",
+	58: "6",
+	59: "7",
+	60: "8",
+	61: "9",
+	62: "-",
+	63: "_",
 }
 
-var defaultDecodeDic = map[string]int64{
+var defaultDecodeDic = map[string]int{
 	"A": 0,
 	"B": 1,
 	"C": 2,
@@ -62,81 +81,97 @@ var defaultDecodeDic = map[string]int64{
 	"F": 5,
 	"G": 6,
 	"H": 7,
-	"J": 8,
-	"K": 9,
-	"M": 10,
-	"N": 11,
-	"P": 12,
-	"Q": 13,
-	"R": 14,
-	"S": 15,
-	"T": 16,
-	"U": 17,
-	"V": 18,
-	"W": 19,
-	"X": 20,
-	"Y": 21,
-	"Z": 22,
-	"a": 23,
-	"b": 24,
-	"c": 25,
-	"d": 26,
-	"e": 27,
-	"f": 28,
-	"g": 29,
-	"h": 30,
-	"j": 31,
-	"k": 32,
-	"m": 33,
-	"n": 34,
-	"p": 35,
-	"q": 36,
-	"r": 37,
-	"s": 38,
-	"t": 39,
-	"u": 40,
-	"v": 41,
-	"x": 42,
-	"y": 43,
-	"z": 44,
-	"2": 45,
-	"3": 46,
-	"4": 47,
+	"I": 8,
+	"J": 9,
+	"K": 10,
+	"L": 11,
+	"M": 12,
+	"N": 13,
+	"O": 14,
+	"P": 15,
+	"Q": 16,
+	"R": 17,
+	"S": 18,
+	"T": 19,
+	"U": 20,
+	"V": 21,
+	"W": 22,
+	"X": 23,
+	"Y": 24,
+	"Z": 25,
+	"a": 26,
+	"b": 27,
+	"c": 28,
+	"d": 29,
+	"e": 30,
+	"f": 31,
+	"g": 32,
+	"h": 33,
+	"i": 34,
+	"j": 35,
+	"k": 36,
+	"l": 37,
+	"m": 38,
+	"n": 39,
+	"o": 40,
+	"p": 41,
+	"q": 42,
+	"r": 43,
+	"s": 44,
+	"t": 45,
+	"u": 46,
+	"v": 47,
+	"w": 48,
+	"x": 49,
+	"y": 50,
+	"z": 51,
+	"0": 52,
+	"1": 53,
+	"2": 54,
+	"3": 55,
+	"4": 56,
+	"5": 57,
+	"6": 58,
+	"7": 59,
+	"8": 60,
+	"9": 61,
+	"-": 62,
+	"_": 63,
 }
 
-// CompresNubmer :
-func CompresNubmer(input int64, dic map[int64]string) string {
+// CompresNumber :
+func CompresNumber(input int64, dic map[int]string) string {
 	b := int64(len(dic))
 	res := ""
 	if input == 0 {
 		return dic[0]
 	}
 	for input > 0 {
-		val := input % b
+		val := int(input % b)
 		input = input / b
 		res += dic[val]
 	}
 	return res
 }
 
-// UncompresNubmer :
-func UncompresNubmer(encoded string, dic map[string]int64) int64 {
+// UncompresNumber :
+func UncompresNumber(encoded string, dic map[string]int) int64 {
 	b := int64(len(dic))
 	res := int64(0)
 	for i := int64(len(encoded)) - 1; i >= 0; i-- {
 		ch := string([]rune(encoded)[i])
-		val := dic[ch]
+		val := int64(dic[ch])
 		res = (res * b) + val
 	}
 	return res
 }
 
-// CompresNubmerDefault :
-func CompresNubmerDefault(input int64) string {
-	return CompresNubmer(input, defaultEncodeDic)
+// CompresNumberDefault :
+func CompresNumberDefault(input int64) string {
+	return CompresNumber(input, defaultEncodeDic)
 }
 
-// UncompresNubmerDefault :
-func UncompresNubmerDefault(input string) int64 {
-	return UncompresNubmer(input, defaultDecodeDic)
+// UncompresNumberDefault :
+func UncompresNumberDefault(input string) int64 {
+	return UncompresNumber(input, defaultDecodeDic)
 }
--- numbercompression/numbercompression_test.go
+++ numbercompression/numbercompression_test.go
@@ -5,20 +5,20 @@ import "testing"
 //!+test
 //go test -v
 
-func TestCompresNubmerUncompresNubmerDefault1000000(t *testing.T) {
+func TestCompresNubmerUncompresNumberDefault1000000(t *testing.T) {
 	for index := int64(0); index < 1000000; index++ {
-		encode := CompresNubmerDefault(index)
-		decode := UncompresNubmerDefault(encode)
+		encode := CompresNumberDefault(index)
+		decode := UncompresNumberDefault(encode)
 		if decode != index {
 			t.Errorf("TestCompresNubmerUncompresNubmer\nindex:%v\nencode:%v\ndecode%v\n", index, encode, decode)
 		}
 	}
 }
 
-func TestCompresNubmerUncompresNubmerDefault1000005000000(t *testing.T) {
+func TestCompresNubmerUncompresNumberDefault1000005000000(t *testing.T) {
 	for index := int64(1000000000000); index < 1000005000000; index++ {
-		encode := CompresNubmerDefault(index)
-		decode := UncompresNubmerDefault(encode)
+		encode := CompresNumberDefault(index)
+		decode := UncompresNumberDefault(encode)
 		if decode != index {
 			t.Errorf("TestCompresNubmerUncompresNubmer\nindex:%v\nencode:%v\ndecode%v\n", index, encode, decode)
 		}
@@ -31,21 +31,21 @@ func TestCompresNubmerUncompresNubmerDefault1000005000000(t *testing.T) {
 //go test -v  -bench=.
 func BenchmarkCompresNubmerUncompresNubmerDefault(b *testing.B) {
 	for index := int64(0); index < int64(b.N); index++ {
-		encode := CompresNubmerDefault(index)
-		decode := UncompresNubmerDefault(encode)
+		encode := CompresNumberDefault(index)
+		decode := UncompresNumberDefault(encode)
 		if decode != index {
-			b.Errorf("BenchmarkCompresNubmerUncompresNubmerDefault\nindex:%v\nencode:%v\ndecode%v\n", index, encode, decode)
+			b.Errorf("BenchmarkCompresNubmerUncompresNumberDefault\nindex:%v\nencode:%v\ndecode%v\n", index, encode, decode)
 		}
 	}
 }
 
-func BenchmarkCompresNubmerUncompresNubmerDefault1000000000(b *testing.B) {
+func BenchmarkCompresNubmerUncompresNumberDefault1000000000(b *testing.B) {
 	for index := int64(0); index < int64(b.N); index++ {
 		i := index + 1000000000000
-		encode := CompresNubmerDefault(i)
-		decode := UncompresNubmerDefault(encode)
+		encode := CompresNumberDefault(i)
+		decode := UncompresNumberDefault(encode)
 		if decode != i {
-			b.Errorf("BenchmarkCompresNubmerUncompresNubmerDefault\nindex:%v\nencode:%v\ndecode%v\n", i, encode, decode)
+			b.Errorf("BenchmarkCompresNubmerUncompresNumberDefault\nindex:%v\nencode:%v\ndecode%v\n", i, encode, decode)
 		}
 	}
 }
