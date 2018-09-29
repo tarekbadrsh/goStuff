6966e2a804505ffee3dd21277c780c8284181811
tarekbadr <tarekbadrshalaan@gmail.com>
2018-09-29 16:06:51 +0200

    add getkeystring

--- jsonparser/jsonparser.go
+++ jsonparser/jsonparser.go
@@ -18,15 +18,34 @@ func JSONParser(data []byte, keys ...string) (map[string]interface{}, error) {
 	if err := json.Unmarshal(data, &objmap); err != nil {
 		return nil, err
 	}
-	return getkey(objmap, keys...)
+	return Getkey(objmap, keys...)
 }
 
-func getkey(o map[string]interface{}, keys ...string) (map[string]interface{}, error) {
+// Getkey : get key from map string recursively
+func Getkey(o map[string]interface{}, keys ...string) (map[string]interface{}, error) {
 	if len(keys) > 1 {
-		return getkey(o[keys[0]].(map[string]interface{}), keys[1:]...)
+		return Getkey(o[keys[0]].(map[string]interface{}), keys[1:]...)
 	}
 	if val, ok := o[keys[0]].(map[string]interface{}); ok {
 		return val, nil
 	}
 	return nil, fmt.Errorf("Unmarshal failed key:%v | value:%v", keys[0], o[keys[0]])
+}
+
+// Getkeystring : get key from map string recursively
+func Getkeystring(o map[string]interface{}, keys ...string) (string, error) {
+	if len(keys) > 1 {
+		if val, ok := o[keys[0]].(map[string]interface{}); ok {
+			return Getkeystring(val, keys[1:]...)
+		} else if val, ok := o[keys[0]].(string); ok {
+			var objmap map[string]interface{}
+			if err := json.Unmarshal([]byte(val), &objmap); err == nil {
+				return Getkeystring(objmap, keys[1:]...)
+			}
+		}
+	}
+	if val, ok := o[keys[0]].(string); ok {
+		return val, nil
+	}
+	return "", fmt.Errorf("Unmarshal failed key:%v | value:%v", keys[0], o[keys[0]])
 }
