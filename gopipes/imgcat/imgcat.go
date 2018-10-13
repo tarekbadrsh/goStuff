218861f88afa4634abac2597e96238df561bfad9
tarekbadr <tarekbadrshalaan@gmail.com>
2018-10-13 12:02:15 +0200

    ch

--- gopipes/imgcat/imgcat.go
+++ gopipes/imgcat/imgcat.go
@@ -13,33 +13,32 @@ import (
 func Copy(w io.Writer, r io.Reader) error {
 	header := strings.NewReader("\033]1337;File=inline=1:")
 	footer := strings.NewReader("\a\n")
+
 	pr, pw := io.Pipe()
 	go func() {
 		defer pw.Close()
+
 		wc := base64.NewEncoder(base64.StdEncoding, pw)
 		_, err := io.Copy(wc, r)
 		if err != nil {
-			pw.CloseWithError(errors.Wrap(err, "Could not encode image"))
+			pw.CloseWithError(errors.Wrap(err, "could not encode image"))
 			return
 		}
+
 		if err := wc.Close(); err != nil {
 			pw.CloseWithError(errors.Wrap(err, "could not close base64 encoder"))
 			return
 		}
-
 	}()
+
 	_, err := io.Copy(w, io.MultiReader(header, pr, footer))
 	return err
-}
-
-type writer struct {
-	pw   *io.PipeWriter
-	done chan struct{}
 }
 
 // NewWriter returns a new imgcat writer.
 func NewWriter(w io.Writer) io.WriteCloser {
 	pr, pw := io.Pipe()
+
 	wc := &writer{pw, make(chan struct{})}
 	go func() {
 		defer close(wc.done)
@@ -47,6 +46,11 @@ func NewWriter(w io.Writer) io.WriteCloser {
 		pr.CloseWithError(err)
 	}()
 	return wc
+}
+
+type writer struct {
+	pw   *io.PipeWriter
+	done chan struct{}
 }
 
 func (w *writer) Write(data []byte) (int, error) {
